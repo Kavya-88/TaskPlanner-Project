fd0ae5bb2be569afc2022434b70b452777a91cac
Kavya <kavya.hrv@gmail.com>
2021-09-22 16:41:43 +1000

    Adding form

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+// function validFormFieldInput() {
+//   var taskname = document.addtask.inputtext;
+//   if(allLetter(taskname)){
+//   }
+//   return false;
+// }
 
+// function allLetter(taskname)
+// { 
+// var letters = /^[A-Za-z]+$/;
+// if(taskname.value.match(letters))
+// {
+// return true;
+// }
+// else
+// {
+// alert('Task name must have alphabet characters only');
+// taskname.focus();
+// return false;
+// }
+// }
+
+
+
+function validFormFieldInput() {
+  if( document.addtask.Name.value == "") {
+     alert( "Please provide your name!");
+     document.addtask.Name.focus();
+     return false;
+  }
+}
