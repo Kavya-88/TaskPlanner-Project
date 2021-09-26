34b72e3047b3fa5d1e72775e4e7e491a2e77ff4f
Uma Bandi <umabandi1988@gmail.com>
2021-09-27 09:54:13 +1000

    task 7 changes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
+const taskLists = new TaskManager(0);
+
 // const form = document.querySelector("#Modal");
+
 let name1 = document.getElementById('myName');
   let validateDescription = document.querySelector("#description");
   const validateAssignedTo = document.querySelector("#assign");
@@ -15,12 +18,16 @@ let errMsg5 = document.querySelector("#errMsg5");
 btnSub2.addEventListener("click", validateBox);
 // let validationFail = 0;
 function validateBox() {
-  
+  let valFail = 0;
+
   // Task Name input value is more than 5 characters.
   if (name1.value == "" || name1.value.length < 5) {
     console.log(name1.value.length);
     errMsg1.innerHTML = "Length should be more than 5";
     errMsg1.style.color = "#ff0000";
+    valFail += 1;
+    console.log("valFail");
+
     name1.focus();
   } else {
     errMsg1.innerHTML = "All good ";
@@ -33,6 +40,7 @@ function validateBox() {
     console.log(validateDescription.value.length);
     errMsg2.innerHTML = "Length should be more than 5";
     errMsg2.style.color = "#ff0000";
+    valFail += 1;
     validateDescription.focus();
     } else {
     errMsg2.innerHTML = "All good ";
@@ -44,21 +52,34 @@ if(validateAssignedTo.value === "assignedto"){
 console.log(validateAssignedTo.value);
   errMsg3.innerHTML ="Choose the name";
   errMsg3.style.color = "#ff0000";
+  valFail += 1;
   validateAssignedTo.focus();
   } else {
   errMsg3.innerHTML = "All good ";
   errMsg3.style.color = "#eeeeee";
 }
+//Duedate
 
+if(!validateDueDate.value){
+  console.log(validateDueDate.value);
+    errMsg4.innerHTML ="Choose the date";
+    errMsg4.style.color = "#ff0000";
+    valFail += 1;
+    validateDueDate.focus();
+    } else {
+    errMsg4.innerHTML = "";
+    validateDueDate.focus();
+  }
 // // validate Status
 if(validateStatus.value === "status"){
 console.log(validateStatus.value);
-  errMsg4.innerHTML ="Choose the status";
-  errMsg4.style.color = "#ff0000";
+  errMsg5.innerHTML ="Choose the status";
+  errMsg5.style.color = "#ff0000";
+  valFail += 1;
   validateStatus.focus();
   } else {
-  errMsg4.innerHTML = "All good ";
-  errMsg4.style.color = "#eeeeee";
+  errMsg5.innerHTML = "All good ";
+  errMsg5.style.color = "#eeeeee";
 }
 
 // console.log(validateStatus.value);
@@ -71,62 +92,106 @@ console.log(validateStatus.value);
   //     errMsg5.innerHTML = "All good ";
   //     errMsg5.style.color = "#eeeeee";
   //   }
-
-  if (validateDueDate.value) {
-    validateDueDate.classList.add("is-valid");
-    validateDueDate.classList.remove("is-invalid");
-  } else {
-    validateDueDate.classList.add("is-invalid");
-    validateDueDate.classList.remove("is-valid");
-    // validationFail++;
+  //*
+//   validateDueDate.onclick = () => {
+//     let now = new Date();
+//     let month = now.getMonth() + 1;
+//     if (month < 10) {
+//         month = "0" + month;
+//     }
+//     let currentDate = now.getFullYear() + "-" + month + "-" + now.getDate();
+//     validateDueDate.setAttribute("min", currentDate);
+// };
+// validateDueDate.addEventListener("focusout", () => {
+//   taskDateData = validateDueDate.value;
+//   errorMessage = " Enter the correct date";
+//   !taskDateData
+//       ? errorMessageGenerator(validateDueDate, spanError[3], "", errorMessage)
+//       : errorMessageStyleReset(validateDueDate, spanError[3]);
+//   updateSubmission();
+// });
+//*************************
+  
+    if (valFail >0) {
+      valFail=0;
+      return;
+    } else {
+      taskLists.addTask(name1.value,validateDescription.value,validateAssignedTo.value,validateDueDate.value,validateStatus.value);
+    console.log(taskLists.tasks)
+    taskLists.render();
   }
+  
+return;
+}
+//Adding Task list step 8 
+const taskcard = document.querySelector("taskcard");
+// Add an 'onclick' event listener to the Tasks List
+taskcard.addEventListener("click",(event) => {
+// Check if a "Mark As Done" button was clicked
+if (event.target.classList.contains("done-button")) {
+   // Get the correct parent Task, yours might be slightly different
+    // Use console.log(event.target.parentElement) to see
+    const parentTask =
+      event.target.parentElement.parentElement.parentElement.parentElement;
+    // Get the taskId of the parent Task and turn it into a number.
+    const taskId = Number(parentTask.dataset.taskId);
+    // Get the task from the TaskManager using the taskId
+    const task = taskManager.getTaskById(taskId);
+    // Update the task status to 'DONE'
+    task.status = "Done";
+    
+
+    // Render the tasks
+    taskManager.render();
+}
+});
+
     
 //     console.log(date.value);
     
-    
-    }
-    
+
+
+
   
-  
-//task 6 step 1
+//task 6 step 1- seperate code
 
     
-    let taskArray = [];
+    // let taskArray = [];
 
-    const addTask = (event) => {
-      event.preventDefault(); //to stop the form submitting
-      let task = {
-        taskName: document.getElementById('myName').value,
-        description: document.getElementById("description").value,
-        assignedTo: document.getElementById("assign").value,
-        dueDate: document.getElementById("dueDate").value,
-        status: document.getElementById("status").value,
+    // const addTask = (event) => {
+    //   event.preventDefault(); //to stop the form submitting
+    //   let task = {
+    //     taskName: document.getElementById('myName').value,
+    //     description: document.getElementById("description").value,
+    //     assignedTo: document.getElementById("assign").value,
+    //     dueDate: document.getElementById("dueDate").value,
+    //     status: document.getElementById("status").value,
         
-      }
+    //   }
+
+        
+
+
+    //   taskArray.push(task);
+    //   document.forms[0].reset(); //to clear the form for the next entries
+    //   // document.querySelector('form').reset();
+
+    //   //for display purpose only
+    //   alert("Task added", {taskArray});
+    //   let pre = document.querySelector('#msg pre');
+    //   pre.textContent = '\n' + JSON.stringify(taskArray, '\t', 2);
+
+    //   //saving to local storage
+      
+    //   // localStorage.setItem('MyTaskList', JSON.stringify(taskArray));
 
       
 
-
-      taskArray.push(task);
-      document.forms[0].reset(); //to clear the form for the next entries
-      //document.querySelector('form').reset();
-
-      //for display purpose only
-      console.log("added", {taskArray});
-      // let pre = document.querySelector('#msg pre');
-      // pre.textContent = '\n' + JSON.stringify(taskArray, '\t', 2);
-
-      //saving to local storage
       
-      // localStorage.setItem('MyTaskList', JSON.stringify(taskArray));
-
-      
-
-      
-      }
-      document.addEventListener('DOMContentLoaded', () => {
-        document.getElementById('savechanges').addEventListener('click', addTask);
-      });
+    //   }
+    //   document.addEventListener('DOMContentLoaded', () => {
+    //     document.getElementById('savechanges').addEventListener('click', addTask);
+    //   });
 
 
 
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,3 +1,29 @@
+//In js/taskManager.js, above the TaskManager class definition, create a new function, createTaskHtml. The function should accept the following parameters:
+
+function createTaskHtml (id, myName, description, assignedto, dueDate, status){
+  //Within this createTaskHtml function, create a string using template literals, copying across one of your tasks that we hardcoded in earlier in task 3 from the index.html
+  //Using the template literal placeholders (${}), replace each text section of the task HTML with the correct parameter
+  const html = `
+  <li class="card" data-task-id="${id}" style="width: 18rem;" id="taskcard">
+  <div class="card-body">
+    <h5 class="card-title">Task</h5>
+    <p class="card-text">Name: ${myName}</p>
+    <p>Description: ${description}</p>
+    <p>Assigned to: ${assignedto}</p>
+    <p> Duedate: ${dueDate}</p>
+    <p> Status: ${status}</p>
+    <button type="button" class="btn btn-success">Edit</button>
+    <button type="button" class="btn btn-danger">Delete</button>
+    <button class="btn btn-outline-success done-button">Done</button>
+      </div>
+</li>`
+
+//Return the HTML from the function
+ return html;
+};
+
+
+
 class TaskManager {
     constructor(currentId = 0) {
       this._tasks = [];
@@ -8,23 +34,80 @@ class TaskManager {
     }
   
     // Create the addTask method
-    addTask(name, description, assignedTo, dueDate, status) {
+    addTask(myName, description, assignedTo, dueDate, status) {
       // Create a task object that we will push to the list of tasks
       
       const newtask = {
-          name: name,
+        id:this.currentId++,
+          name:myName,
           description: description,
           assignedTo: assignedTo,
           dueDate: dueDate,
           status: status,
       };
+    
   // Increment the current Id for each new task
-      this.currentId++,
-
+      // this.currentId++,
+     
       this.tasks.push(newtask);
+      document.forms[0].reset();
+      alert("Task added", {newtask})
     }
+
+    getTaskById(taskId) {
+      // Create a variable to store the found task
+      let foundTask;
+      // Loop over the tasks and find the task with the id passed as a parameter
+      for (let i = 0; i < this.tasks.length; i++) {
+        // Get the current task in the loop
+        const task = this.tasks[i];
+        // Check if its the right task by comparing the task's id to the id passed as a parameter
+        if (task.id === taskId) {
+          // Store the task in the foundTask variable
+          foundTask = task;
+        }
+      }
+      // Return the found task
+      return foundTask;
+    }
+    //In js/taskManager.js, within the TaskManager class, create a render() method. This method does not need any parameters.
+    //Create a variable tasksHtmlList and assign it an empty array. This will hold the HTML of all the tasks.
+    render() {
+      let taskHtmlList = [];
+      // Loop over our tasks and create the html, storing it in the array
+    for (let i = 0; i < this.tasks.length; i++) {
+      // Get the current task in the loop
+      const newtask = this.tasks[i];
+      // Format the date
+      const date = new Date(newtask.dueDate);
+      const formattedDate =
+        date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+      // Create the task html
+      const taskHtml = createTaskHtml(
+        newtask.id,
+        newtask.name,
+        newtask.description,
+        newtask.assignedTo,
+        formattedDate,
+        newtask.status
+      );
+      // Push it to the tasksHtmlList array
+      taskHtmlList.push(taskHtml);
+      }
+    // Create the tasksHtml by joining each item in the tasksHtmlList
+    // with a new line in between each item.
+    const tasksHtml = taskHtmlList.join("\n");
+
+    // Set the inner html of the tasksList on the page
+    const taskLists = document.querySelector("#taskcard");
+    taskLists.innerHTML = tasksHtml;
+    }
+    
   }
+  
   // let taskList = new TaskManager();
   // taskList.addTasks(name, description, assignedTo, dueDate, status);
   // console.log(taskList.tasks);
+
+ 
 
